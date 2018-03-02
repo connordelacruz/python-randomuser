58aaf6c58a27eae01c42071d3d6c7051cf2c421d
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-02 15:50:33 -0600

    Updated tag

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='randomuser',
         packages=['randomuser'],
-        version='0.1',
+        version='0.1.1',
         description='Python class for generating random user data using https://randomuser.me API',
         url='https://github.com/connordelacruz/python-randomuser',
-        download_url='https://github.com/connordelacruz/python-randomuser/archive/0.1.tar.gz',
+        download_url='https://github.com/connordelacruz/python-randomuser/archive/0.1.1.tar.gz',
         author='Connor de la Cruz',
         author_email='connor.c.delacruz@gmail.com',
         license='MIT',
