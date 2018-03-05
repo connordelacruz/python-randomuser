36d8f3cbd97b0e08300d741354af39c40bae6ffd
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-05 14:48:07 -0600

    Made data and info instance variables private and updated docs

--- randomuser/randomuser.py
+++ randomuser/randomuser.py
@@ -13,26 +13,27 @@ import re
 
 URL = 'https://randomuser.me/api/'
 
+
 # Classes
 # ----------------------------------------------------------------
 
 class RandomUser(object):
-    # Dictionary where the random user data will be stored
-    data = {}
-    # Dictionary where info section of results will be stored
-    info = {}
+    # Dictionary where the random user _data will be stored
+    _data = {}
+    # Dictionary where _info section of results will be stored
+    _info = {}
 
     def __init__(self, get_params=None, user_data=None, api_info=None):
         """Initialize RandomUser object
 
         :param get_params: (Optional) Dictionary mapping query parameter names to their values. See https://randomuser.me/documentation for details on parameters.
-        :param user_data: (Optional) If specified, this data will be used instead of querying the API for user data. Use in instances where the user data has already been generated (e.g. restoring user data, creating multiple users with single call to API using the 'results' parameter)
-        :param api_info: (Optional) If the user is being generated with the user_data parameter, the info variable will be set to this. Otherwise, it will be ignored when generating a random user.
+        :param user_data: (Optional) If specified, this _data will be used instead of querying the API for user _data. Use in instances where the user _data has already been generated (e.g. restoring user _data, creating multiple users with single call to API using the 'results' parameter)
+        :param api_info: (Optional) If the user is being generated with the user_data parameter, the _info variable will be set to this. Otherwise, it will be ignored when generating a random user.
         """
         global URL
         if user_data is not None:
-            self.data = user_data
-            self.info = api_info
+            self._data = user_data
+            self._info = api_info
         else:
             self.request_url = URL
             if get_params:
@@ -40,11 +41,10 @@ class RandomUser(object):
             self._generate_user()
 
     def _generate_user(self):
-        # TODO: catch timeout exception and fall back to local data?
+        # TODO: catch timeout exception and fall back to local _data?
         results = json.loads(request.urlopen(self.request_url).read())
-        self.data = results['results'][0]
-        self.info = results['info']
-
+        self._data = results['results'][0]
+        self._info = results['_info']
 
     # Personal Info
     # --------------------------------
@@ -54,7 +54,7 @@ class RandomUser(object):
 
         :param capitalize: (Default = True) Capitalize first letter if True
         """
-        first_name = self.data['name']['first']
+        first_name = self._data['name']['first']
         return first_name.title() if capitalize else first_name
 
     def get_last_name(self, capitalize=True):
@@ -62,7 +62,7 @@ class RandomUser(object):
 
         :param capitalize: (Default = True) Capitalize first letter if True
         """
-        last_name = self.data['name']['last']
+        last_name = self._data['name']['last']
         return last_name.title() if capitalize else last_name
 
     def get_full_name(self, capitalize=True):
@@ -77,39 +77,39 @@ class RandomUser(object):
 
     def get_gender(self):
         """Returns gender"""
-        return self.data['gender']
+        return self._data['gender']
 
     def get_dob(self, parse_time=False):
         """Returns date of birth as a string in the format '%Y-%m-%d %H:%M:%S'
 
         :param parse_time: (Default = False) If True, parse date of birth string using time.strptime() and return the results instead of a string
         """
-        dob = self.data['dob']
+        dob = self._data['dob']
         if parse_time:
             dob = self._parse_time(dob)
         return dob
 
     def get_nat(self):
         """Returns nationality"""
-        return self.data['nat']
+        return self._data['nat']
 
     # Location
     # --------------------------------
 
     def get_street(self):
         """Returns street address"""
-        return self.data['location']['street']
+        return self._data['location']['street']
 
     def get_city(self):
         """Returns city"""
-        return self.data['location']['city']
+        return self._data['location']['city']
 
     def get_state(self):
-        return self.data['location']['state']
+        return self._data['location']['state']
 
     def get_postcode(self):
         """Returns post code"""
-        return self.data['location']['postcode']
+        return self._data['location']['postcode']
 
     def get_zipcode(self):
         """ Returns zip code (wrapper for get_postcode())"""
@@ -137,7 +137,7 @@ class RandomUser(object):
         :param strip_parentheses: (Default = False) Omit parentheses if True
         :param strip_hyphens: (Default = False) Omit hyphens if True
         """
-        return self._format_phone_number(self.data['phone'], strip_parentheses, strip_hyphens)
+        return self._format_phone_number(self._data['phone'], strip_parentheses, strip_hyphens)
 
     def get_cell(self, strip_parentheses=False, strip_hyphens=False):
         """Returns cell phone number as a string in the format '(###)-###-####'
@@ -145,64 +145,64 @@ class RandomUser(object):
         :param strip_parentheses: (Default = False) Omit parentheses if True
         :param strip_hyphens: (Default = False) Omit hyphens if True
         """
-        return self._format_phone_number(self.data['cell'], strip_parentheses, strip_hyphens)
+        return self._format_phone_number(self._data['cell'], strip_parentheses, strip_hyphens)
 
     def get_email(self):
         """Returns email address"""
-        return self.data['email']
+        return self._data['email']
 
     # Login
     # --------------------------------
 
     def get_username(self):
         """Returns username"""
-        return self.data['login']['username']
+        return self._data['login']['username']
 
     def get_password(self):
         """Returns password"""
-        return self.data['login']['password']
+        return self._data['login']['password']
 
     def get_registered(self, parse_time=False):
         """Returns registration date as a string in the format '%Y-%m-%d %H:%M:%S'
 
         :param parse_time: (Default = False) If True, parse date string using time.strptime() and return the results instead of a string
         """
-        registered = self.data['registered']
+        registered = self._data['registered']
         if parse_time:
             registered = self._parse_time(registered)
         return registered
 
     def get_login_salt(self):
         """Returns user login salt"""
-        return self.data['login']['salt']
+        return self._data['login']['salt']
 
     def get_login_md5(self):
         """Returns user login md5"""
-        return self.data['login']['md5']
+        return self._data['login']['md5']
 
     def get_login_sha1(self):
         """Returns user login sha1"""
-        return self.data['login']['sha1']
+        return self._data['login']['sha1']
 
     def get_login_sha256(self):
         """Returns user login sha256"""
-        return self.data['login']['sha256']
+        return self._data['login']['sha256']
 
     # ID
     # --------------------------------
 
     def get_id_type(self):
         """Returns the ID type"""
-        return self.data['id']['name']
+        return self._data['id']['name']
 
     def get_id_number(self):
         """Returns the ID number"""
-        return self.data['id']['value']
+        return self._data['id']['value']
 
     def get_id(self):
         """Returns a dictionary mapping 'type' to ID type and 'number' to ID number
         """
-        return {'type': self.get_id_type(), 'number': self.get_id_(number)}
+        return {'type': self.get_id_type(), 'number': self.get_id_number()}
 
     # Misc
     # --------------------------------
@@ -217,9 +217,9 @@ class RandomUser(object):
 
         :param size: (Default = PICTURE_SIZE_LARGE) The size of picture to return the url for. Possible values for this are stored in constants PICTURE_SIZE_LARGE, PICTURE_SIZE_MEDIUM, and PICTURE_SIZE_THUMBNAIL
         """
-        return self.data['picture'][size]
+        return self._data['picture'][size]
 
-    # Constants for info dictionary keys
+    # Constants for _info dictionary keys
     INFO_SEED = 'seed'
     INFO_RESULTS = 'results'
     INFO_PAGE = 'page'
@@ -228,10 +228,9 @@ class RandomUser(object):
     def get_info(self):
         """Returns a dictionary with information about the API query
 
-        Keys for the info dictionary are stored in constants INFO_SEED, INFO_RESULTS, INFO_PAGE, and INFO_VERSION
+        Keys for the _info dictionary are stored in constants INFO_SEED, INFO_RESULTS, INFO_PAGE, and INFO_VERSION
         """
-        return self.info
-
+        return self._info
 
     # Helper Functions
     # --------------------------------
@@ -243,7 +242,6 @@ class RandomUser(object):
         """
         date_format = '%Y-%m-%d %H:%M:%S'
         return time.strptime(date_string, date_format)
-
 
     # Static Methods
     # --------------------------------
@@ -265,11 +263,9 @@ class RandomUser(object):
         get_params['results'] = amount if amount <= 5000 else 5000
         request_url = URL + '?' + urlencode(get_params)
         results = json.loads(request.urlopen(request_url).read())
-        info = results['info']
+        info = results['_info']
         users = []
         for user_data in results['results']:
             user = RandomUser(user_data=user_data, api_info=info)
             users.append(user)
         return users
-
-
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='randomuser',
         version='0.1.1',
-        description='Python class for generating random user data using https://randomuser.me API',
+        description='Python class for generating random user _data using https://randomuser.me API',
         url='https://github.com/connordelacruz/python-randomuser',
         download_url='https://github.com/connordelacruz/python-randomuser/archive/0.1.1.tar.gz',
         author='Connor de la Cruz',
