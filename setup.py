e4c2ef4ff7ee093716118ae1d0f1efcad594b4f6
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-05 14:49:23 -0600

    Updated setup.py version number to 0.2.0

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='randomuser',
-        version='0.1.1',
+        version='0.2.0',
         description='Python class for generating random user _data using https://randomuser.me API',
         url='https://github.com/connordelacruz/python-randomuser',
-        download_url='https://github.com/connordelacruz/python-randomuser/archive/0.1.1.tar.gz',
+        download_url='https://github.com/connordelacruz/python-randomuser/archive/0.2.0.tar.gz',
         author='Connor de la Cruz',
         author_email='connor.c.delacruz@gmail.com',
         license='MIT',
