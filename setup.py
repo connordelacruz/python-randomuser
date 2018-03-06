a934345ab658f7963fcf85418dbb63409c9b1bd8
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-06 08:36:21 -0600

    Fixed a typo in setup.py description
    Did not realize IDE refactoring to make info and data private instance
    variables would be so aggressive that it changed string instances of
    'data' and 'info' to include an underscore as well.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='randomuser',
         version='0.3.0',
-        description='Python class for generating random user _data using https://randomuser.me API',
+        description='Python class for generating random user data using https://randomuser.me API',
         url='https://github.com/connordelacruz/python-randomuser',
         download_url='https://github.com/connordelacruz/python-randomuser/archive/0.3.0.tar.gz',
         author='Connor de la Cruz',
