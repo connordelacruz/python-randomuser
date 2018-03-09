c216cb16f78692d0a99410d2c2427f4f49192e3b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-09 15:49:09 -0600

    Added version.py so version number can be updated in one place

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
+# Get __version__
+with open('./randomuser/version.py') as f:
+    exec(f.read())
+
 setup(name='randomuser',
-        version='0.3.1',
-        description='Python class for generating random user data using https://randomuser.me API',
-        url='http://connordelacruz.com/python-randomuser/',
-        download_url='https://github.com/connordelacruz/python-randomuser/archive/0.3.1.tar.gz',
-        author='Connor de la Cruz',
-        author_email='connor.c.delacruz@gmail.com',
-        license='MIT',
-        packages=find_packages(),
-        zip_safe=False)
+      version=__version__,
+      description='Python class for generating random user data using https://randomuser.me API',
+      url='http://connordelacruz.com/python-randomuser/',
+      download_url='https://github.com/connordelacruz/python-randomuser/archive/{}.tar.gz'.format(__version__),
+      author='Connor de la Cruz',
+      author_email='connor.c.delacruz@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
