49a3cd5038828b236e6a7e2d747f750953895c4e
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-04-19 13:02:00 -0500

    Fixed the setup issue
    setup.py wasn't properly updated after converting package to a single
    module. Fixed the issues and tested, then bumped version to 1.4.0

--- randomuser.py
+++ randomuser.py
@@ -12,7 +12,7 @@ import re
 # ----------------------------------------------------------------
 
 # Python module version
-__version__ = '1.3.0'
+__version__ = '1.4.0'
 
 # Version of the random user API
 API_VERSION = '1.1'
--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-setup_dir = os.path.abspath(os.path.dirname(__file__))
 
 def readme():
-    with open(os.path.join(setup_dir, 'README.rst')) as f:
+    with open('README.rst') as f:
         return f.read()
 
 # Get __version__
-with open(os.path.join(setup_dir, 'randomuser.py')) as f:
+with open('randomuser.py') as f:
     exec(f.read())
 
 setup(name='randomuser',
@@ -20,5 +18,5 @@ setup(name='randomuser',
       author='Connor de la Cruz',
       author_email='connor.c.delacruz@gmail.com',
       license='MIT',
-      packages=find_packages(),
+      py_modules=['randomuser'],
       zip_safe=False)
