4282e3bc14d6219e9ea8c1750d77bdab5e1932f1
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-04-19 11:49:20 -0500

    Added necessary files to manifest

--- randomuser.py
+++ randomuser.py
@@ -12,7 +12,7 @@ import re
 # ----------------------------------------------------------------
 
 # Python module version
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 # Version of the random user API
 API_VERSION = '1.1'
